dcd0541ab62fac7a86cf4617b254d88fbcbf4215
Jean Vasconcelos <jeanjunior09@gmail.com>
2022-07-16 16:44:45 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
+import NotificationButton from "./components/notification-button";
+
 function App() {
-
   return (
-    <h1> Hello World</h1>
-
-  )  
+    <>
+      <h1> Hello World</h1>
+      <NotificationButton />
+    </>
+  );
 }
 
-export default App
+export default App;
